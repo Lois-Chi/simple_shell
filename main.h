2819610fe540f9388c5611d89ffdc0dee45e20c3
Lois Onyeanuforo <loisonyeanuforo@gmail.com>
2022-09-08 14:42:25 -0700

    Files added

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#endif
